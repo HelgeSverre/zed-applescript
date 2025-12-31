992662d801c6216a94c8fe54cb7bc07f0ba91f55
Helge Sverre <helge.sverre@gmail.com>
2025-12-31 22:00:50 +0100

    Add runnables support for AppleScript
    - Added runnables.scm to tag handlers and scripts as runnable
    - Added tasks.json with osascript commands to run scripts

---
+++ languages/applescript/runnables.scm
@@ -0,0 +1,6 @@
+; Tag handler definitions as runnable
+(handler_definition
+  name: (identifier) @run) @applescript-handler
+
+; Tag the entire source file as runnable (run whole script)
+(source_file) @applescript-script
