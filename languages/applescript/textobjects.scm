c9886376613c8d9a597156a1071926d3f167f8ed
Helge Sverre <helge.sverre@gmail.com>
2025-12-31 21:39:04 +0100

    Fix textobjects.scm to use only supported capture names

--- languages/applescript/textobjects.scm
+++ languages/applescript/textobjects.scm
@@ -5,18 +5,6 @@
 (comment) @comment.around
 (comment) @comment.inside
 
-; Strings
-(string) @string.around
-(string) @string.inside
-
 ; Functions/handlers
 (handler_definition) @function.around
-(handler_definition
-  name: (identifier)
-  (parameter_list)? @function.inside)
-
-; Blocks
-(tell_block) @block.around
-(if_block) @block.around
-(repeat_block) @block.around
-(try_block) @block.around
+(handler_definition) @function.inside
